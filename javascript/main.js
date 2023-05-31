9dd56afaf58efceecf98892f083547612658be9d
ale <AROrtega@hotmail.com>
2023-05-31 16:09:59 -0300

    nueva funciona finalizar compra, ajuste de clases y diseño, banner, swal en finalizar compra, organizacion botones de accion, nuevo atributo de los items Categoria, exhibido en el inicio, colores adaptados desde standares de BS

--- javascript/main.js
+++ javascript/main.js
@@ -1,28 +1,29 @@
 //Plantilla de Items
 class Item {
-    constructor(id, nombre, precio, img) {
+    constructor(id, nombre, precio, img, categoria) {
         this.id = id;
         this.nombre = nombre;
         this.precio = precio;
         this.img = img;
         this.cantidad = 1;
+        this.categoria = categoria;
     }
 };
 
 //Listado de Item
-const bluelock6 = new Item(1, "Bluelock 6", 2100, "img/bluelock6.jpg");
-const bluelock8 = new Item(2, "Bluelock 8", 2400, "img/bluelock8.jpg");
-const darling2 = new Item(3, "Darling in the Franxx 2", 3750, "img/darling2.jpg");
-const dorohedoro3 = new Item(4, "Dorohedoro 3", 1900, "img/dorohedoro3.jpg");
-const jigokuraku5 = new Item(5, "Jigokuraku 5", 1500, "img/jigokuraku5.jpg");
-const mieruko1 = new Item(6, "Mieruko 1", 4300, "img/mieruko1.jpg");
-const mieruko2 = new Item(7, "Mieruko 2", 4550, "img/mieruko2.jpg");
-const mobpsycho3 = new Item(8, "Mob Psycho 3", 2570, "img/mobpsycho3.jpg");
-const mushihime2 = new Item(9, "Mushishime 2", 3100, "img/mushihime2.jpg");
-const paradise2 = new Item(10, "Paradise 2", 2000, "img/paradise2.jpg");
-const tokyo3 = new Item(11, "Tokyo Revengers 3", 6300, "img/tokyo3.jpg");
-const tokyo4 = new Item(12, "Tokyo Revengers 4", 6500, "img/tokyo4.jpg");
-const tokyo5 = new Item(13, "Tokyo Revengers 5", 6700, "img/tokyo5.jpg");
+const bluelock6 = new Item(1, "Bluelock 6", 2100, "img/bluelock6.jpg", "Deporte");
+const bluelock8 = new Item(2, "Bluelock 8", 2400, "img/bluelock8.jpg", "Deporte");
+const darling2 = new Item(3, "Darling in the Franxx 2", 3750, "img/darling2.jpg", "Drama");
+const dorohedoro3 = new Item(4, "Dorohedoro 3", 1900, "img/dorohedoro3.jpg", "Terror");
+const jigokuraku5 = new Item(5, "Jigokuraku 5", 1500, "img/jigokuraku5.jpg", "Terror");
+const mieruko1 = new Item(6, "Mieruko 1", 4300, "img/mieruko1.jpg", "Terror");
+const mieruko2 = new Item(7, "Mieruko 2", 4550, "img/mieruko2.jpg", "Terror");
+const mobpsycho3 = new Item(8, "Mob Psycho 3", 2570, "img/mobpsycho3.jpg", "Shonen");
+const mushihime2 = new Item(9, "Mushishime 2", 3100, "img/mushihime2.jpg", "Romance");
+const paradise2 = new Item(10, "Paradise 2", 2000, "img/paradise2.jpg", "Terror");
+const tokyo3 = new Item(11, "Tokyo Revengers 3", 6300, "img/tokyo3.jpg", "Drama");
+const tokyo4 = new Item(12, "Tokyo Revengers 4", 6500, "img/tokyo4.jpg", "Drama");
+const tokyo5 = new Item(13, "Tokyo Revengers 5", 6700, "img/tokyo5.jpg", "Drama");
 
 //Array con todos los Items
 const items = [bluelock6, bluelock8, darling2, dorohedoro3, jigokuraku5, mieruko1, mieruko2, mobpsycho3, mushihime2, paradise2, tokyo3, tokyo4, tokyo5];
@@ -42,12 +43,13 @@ const contenedorItems = document.getElementById("contenedorItems");
 const crearItems = () => {
     items.forEach(item => {
         const card = document.createElement("div");
-        card.classList.add("col-xl-3", "col-md-6", "col-xs-12");
+        card.classList.add("col-xl-3", "col-md-6", "col-xs-6");
         card.innerHTML = `
                 <div class="card m-2 cartaProducto">
                     <img class="card-img-tom m-3" src="${item.img}" alt="${item.nombre}">
                     <div class="card-body">
-                        <h4 class="cartaNombre">${item.nombre}</h4>
+                        <h4>${item.nombre}</h4>
+                        <p>Categoria: ${item.categoria}</p>
                         <p>Precio: $${item.precio}</p>
                     </div>
                     <button class="btn btn-info" id="botonAdd${item.id}">Agregar al Carrito</button>
@@ -101,16 +103,16 @@ const insertCarrito = () => {
     contenedorCarrito.innerHTML = "";
     carrito.forEach(item => {
         const card = document.createElement("div");
-        card.classList.add("col-xl-3", "col-md-6", "col-xs-12");
+        card.classList.add("col-xl-3", "col-md-6", "col-xs-6");
         card.innerHTML = `
-                <div class="card">
-                    <img class="card-img-tom" src="${item.img}" alt="${item.nombre}">
+                <div class="card m-2 cartaProducto">
+                        <img class="card-img-tom m-3" src="${item.img}" alt="${item.nombre}">
                     <div class="card-body">
                         <h4>${item.nombre}</h4>
                         <p>Precio unidad: $${item.precio}</p>
                         <p>Cantidad: ${item.cantidad}</p>
                     </div>
-                    <button class="btn btn-danger" id="delete${item.id}">Eliminar</button>
+                        <button class="btn btn-danger" id="delete${item.id}">Eliminar</button>
                 </div>`
         contenedorCarrito.appendChild(card);
         const boton = document.getElementById(`delete${item.id}`);
@@ -148,9 +150,9 @@ clearCarrito.addEventListener("click", () => {
         title: "¿Estás seguro/a?",
         text: "El carrito se vaciará por completo",
         confirmButtonColor: "#dc3545",
-        confirmButtonText: "Vaciar",
+        confirmButtonText: "VACIAR",
         showCancelButton: true,
-        cancelButtonText: "Cancelar"
+        cancelButtonText: "CANCELAR"
     }).then((result) => {
         if(result.isConfirmed) {
             clearAllCarrito();
@@ -165,6 +167,38 @@ clearCarrito.addEventListener("click", () => {
 
 //Función que vacia e inica de cero el carrito
 const clearAllCarrito = () => {
+    carrito = [];
+    localStorage.clear();
+    insertCarrito();
+}
+
+const comprarCarrito = document.getElementById("comprarCarrito");
+comprarCarrito.addEventListener("click", () => {
+    Swal.fire({
+        icon: "question",
+        title: "¿Queres finalizar la compra?",
+        text: "Tu carrito se vé genial, ¡Adelante!",
+        confirmButtonColor: "#157347",
+        confirmButtonText: "FINALIZAR",
+        showCancelButton: true,
+        cancelButtonColor: "#0dcaf0",
+        cancelButtonText: "SEGUIR COMPRANDO"
+    }).then((result) => {
+        if(result.isConfirmed) {
+            finalizarCarrito();
+            Swal.fire({
+                icon: "success",
+                title: "COMPRA CON ÉXITO",
+                text: "Muchas gracias por elegirnos. No olvides seguirnos en nuestras redes.",
+                confirmButtonColor: "#0dcaf0",
+                confirmButtonText: "VOLVER AL INICIO"
+            })
+        }
+    })
+})
+
+//Función que simula finalizar la compra (es igual a clearAllCarrito pero me parece mejor trabajarlas por separado)
+const finalizarCarrito = () => {
     carrito = [];
     localStorage.clear();
     insertCarrito();
